68b2f83d71148a4ac8cd0007de46ed82fa047b32
tannevaled <tannevaled@users.noreply.github.com>
2024-10-17 17:32:33 +0200

    wip

--- terraform__outputs.tf
+++ terraform__outputs.tf
@@ -1,4 +1,6 @@
 output outputs {
     description = "the deployed volumes"
-    value = jsonencode(element(openstack_blockstorage_volume_v3.volume.*, 0))
+    value = {
+        for k, v in openstack_blockstorage_volume_v3.volume : k => v
+    }
 }
