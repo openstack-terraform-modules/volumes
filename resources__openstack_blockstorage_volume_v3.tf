0cc119c42f3c3957924817c7f3aace01df91bb03
tannevaled <tannevaled@users.noreply.github.com>
2024-10-17 16:30:10 +0200

    wip

--- resources__openstack_blockstorage_volume_v3.tf
+++ resources__openstack_blockstorage_volume_v3.tf
@@ -1,9 +1,7 @@
 resource openstack_blockstorage_volume_v3 data {
   count = local.volume_count
 
-  name = format(local.volume_name,
-    count.index+1
-  )
+  name = format(local.volume_name, count.index+1)
   
   size = local.volume_size
 
@@ -12,6 +10,6 @@ resource openstack_blockstorage_volume_v3 data {
     "DEPLOYMENT_ID": local.deployment.id,
     "VOLUME_NAME": format(local.volume_name, count.index+1)
     "VOLUME_DOMAIN": local.domain_name,
-    "VOLUME_FQDN": join(".",format(local.volume_name, count.index+1), local.domain_name)
+    "VOLUME_FQDN": join(".",[format(local.volume_name, count.index+1), local.domain_name])
   }
 }
